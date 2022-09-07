06bb6dc23385ff8fc1d40e26d18af5847d14d28e
wsl_tsyi <tsyi@kisti.re.kr>
2022-09-07 15:21:38 +0900

    경로 수정

---
+++ tsyi_lmlib/setup.py
@@ -0,0 +1,17 @@
+from gettext import install
+from setuptools import setup
+
+setup(
+    name='tsyi_lmlib',
+    version='0.0.0',
+    description='tsyi pip install test',
+    url='https://github.com/taeseokyi/bert-ner-tf',
+    author='taeseok yi',
+    author_email='tsyi@kisti.re.kr',
+    license='taeseok yi',
+    packages=['tsyi_lmlib'],
+    zip_safe=False,
+    install_requires=[
+        'numpy==1.19.2'
+    ]
+)
